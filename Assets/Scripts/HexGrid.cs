c01ee849ef2771117972ba4f83f3a1e4e4067d5a
xdedzl <32391533+xdedzl@users.noreply.github.com>
2018-10-28 23:09:11 +0800

    Initial commit

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour {
+
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+
+    private HexCell[] cells;
+
+    private void Awake()
+    {
+        cells = new HexCell[height * width];
+
+        for (int z = 0,i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreatCell(x, z, i); 
+            }
+        }
+    }
+
+    private void CreatCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = x * 10f;
+        position.y = 0f;
+        position.z = z * 10f;
+
+        HexCell cell = cells[i] = Instantiate(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+    }
+}
---
+++ Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 六边形度量值   
+/// </summary>
+public static class HexMetrics {
+
+    /// <summary>
+    /// 六边形外径
+    /// </summary>
+    public const float outerRadius = 10.0f;
+    /// <summary>
+    /// 六边形内径  内径 = 3^0.5 * 0.5 * 外径
+    /// </summary>
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+    public static Vector3[] corners = {
+    new Vector3(0f, 0f, outerRadius),
+    new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+    new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+    new Vector3(0f, 0f, -outerRadius),
+    new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+    new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+    };  
+}
